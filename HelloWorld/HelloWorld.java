8082deca96a1c73514763d424be01c9f96cc7b22
luckxiang <1049242886@qq.com>
2018-03-22 15:27:56 +0800

    helloworld

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: HelloWorld.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年03月22日 星期四 15时05分56秒
+ ************************************************************************/
+
+import java.util.*;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World\n");
+    }
+}
